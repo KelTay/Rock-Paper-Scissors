f9f3178f8db7f67454c6443dc9fbfe674bd03964
kelvin <klvno.tay@gmail.com>
2019-05-01 15:09:23 -0700

    Add code for playRound function

--- script.js
+++ script.js
@@ -7,16 +7,45 @@ function computerPlay() {
     let random = Math.floor(Math.random() * 3) + 1;
 
     if (random === 1) {
-        return "Rock";
+        return "rock";
     }
 
     if (random === 2) {
-        return "Paper";
+        return "paper";
     }
 
-    return "Scissors";
+    return "scissors";
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    let playerSelectionLowerCase = playerSelection.toLowerCase();
+    const computerWinMessage = "Computer Wins!";
+    const playerWinMessage = "You Win!"
+
+    if (playerSelectionLowerCase === computerSelection) {
+
+        return "It's a tie! Player and Computer both chose "
+                + computerSelection;
+                
+    } else if (playerSelectionLowerCase === "rock") {
+        
+        if (computerSelection  === "paper") {
+            return computerWinMessage + " Paper beats Rock";
+        } else {
+            return playerWinMessage + " Rock beats Scissors";
+        }
+
+    } else if (playerSelectionLowerCase === "paper") {
+        if (computerSelection  === "scissors") {
+            return computerWinMessage + " Scissors beats Paper";
+        } else {
+            return playerWinMessage + " Paper beats Rock";
+        }
+    } else { // Player chooses scissors
+        if (computerSelection  === "rock") {
+            return computerWinMessage + " Rock beats Scissors";
+        } else {
+            return playerWinMessage + " Scissors beats Paper";
+        }
+    }
 }
